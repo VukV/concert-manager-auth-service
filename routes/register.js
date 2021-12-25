f9114d7c3dcd3cf5f641241490f6b632fba3c41a
VukV <vuk.vukovic.2000@gmail.com>
2021-12-25 14:33:47 +0100

    minor changes

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fetch = require('cross-fetch');
+const Joi = require('joi');
 require('dotenv').config();
 
 const route = express.Router();
